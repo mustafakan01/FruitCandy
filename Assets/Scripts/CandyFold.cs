2546fc0ee59b8b9583c35052a751b1fac8d00caf
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-09-14 15:48:47 +0300

    Finish

--- Assets/Scripts/CandyFold.cs
+++ Assets/Scripts/CandyFold.cs
@@ -5,35 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class CandyFold : MonoBehaviour
 {
-    public float x, y;
-    
-    public string Colour;
+    public float x, y; // Şekerin konum koordinatları
+    public string Colour; // Şekerin rengi
 
-    private GameObject secim;
-    private Animator animator;
+    private GameObject secim; // Seçim nesnesi
+    private Animator animator; // Animator bileşeni
 
-    public bool locationChance = false;
-    private bool fall = true;
+    public bool locationChance = false; // Şekerin hareket etme izni
+    private bool fall = true; // Şekerin düşme durumu
 
-    public static CandyFold firstCandy;
-    public static CandyFold secondCandy;
+    public static CandyFold firstCandy; // İlk seçilen şeker
+    public static CandyFold secondCandy; // İkinci seçilen şeker
 
-    public Vector3 targetLocation;
+    public Vector3 targetLocation; // Şekerin gitmeye çalıştığı hedef konum
 
-    private List<CandyFold> candy_x = new List<CandyFold>();
-    private List<CandyFold> candy_y = new List<CandyFold>();
+    private List<CandyFold> candy_x = new List<CandyFold>(); // Şekerlerin yatay sıralaması
+    private List<CandyFold> candy_y = new List<CandyFold>(); // Şekerlerin dikey sıralaması
 
     private void Start()
     {
-        secim = GameObject.FindGameObjectWithTag("Secim");
-        animator = GetComponent<Animator>();
+        secim = GameObject.FindGameObjectWithTag("Secim"); // "Secim" etiketini taşıyan nesneyi al
+        animator = GetComponent<Animator>(); // Animator bileşenini al
     }
 
     private void Update()
     {
         if (fall)
         {
-            locationChance=false;
+            locationChance = false; // Şeker düşerken hareket etme izni kapat
+
+            // Şekerin hedef konumuna yavaşça ilerlemesi
             if (Mathf.Abs(transform.position.y - y) < 0.01f)
             {
                 fall = false;
@@ -44,33 +45,35 @@ public class CandyFold : MonoBehaviour
 
         if (locationChance)
         {
-            LocChance();
+            LocChance(); // Şekerin hedef konumuna gitmesi için işlevi çağır
         }
     }
 
- public void NewLoc(float _x, float _y)
-{
-    // Eski konumu temizle
-    creator.candies[(int)x, (int)y] = null;
-
-    // Yeni konumu ayarla
-    x = _x;
-    y = _y;
-
-    // Yeni konumu güncelle
-    creator.candies[(int)x, (int)y] = this;
-
-    // Şekerin düşme durumunu sıfırla
-    fall = true;
-}
-
-
-    private void OnMouseDown()
+    // Şekerin yeni konumunu ayarlar
+    public void NewLoc(float _x, float _y)
     {
-        secim.transform.position = transform.position;
-        CandyControl();
+        // Eski konumu temizle
+        creator.candies[(int)x, (int)y] = null;
+
+        // Yeni konumu ayarla
+        x = _x;
+        y = _y;
+
+        // Yeni konumu güncelle
+        creator.candies[(int)x, (int)y] = this;
+
+        // Şekerin düşme durumunu sıfırla
+        fall = true;
     }
 
+    // Fare tıklaması algılandığında çalışır
+    private void OnMouseDown()
+    {
+        secim.transform.position = transform.position; // Seçim nesnesinin pozisyonunu güncelle
+        CandyControl(); // Şeker kontrolünü yap
+    }
+
+    // Şeker kontrolünü yapar
     private void CandyControl()
     {
         if (firstCandy == null)
@@ -94,15 +97,15 @@ public class CandyFold : MonoBehaviour
                     firstCandy.locationChance = true;
                     secondCandy.locationChance = true;
 
-                    Chance();
+                    Chance(); // Şekerlerin yerlerini değiştir
 
-                    firstCandy.ControlX();
-                    firstCandy.ControlY();
-                    secondCandy.ControlX();
-                    secondCandy.ControlY();
+                    firstCandy.ControlX(); // X ekseni kontrolünü yap
+                    firstCandy.ControlY(); // Y ekseni kontrolünü yap
+                    secondCandy.ControlX(); // X ekseni kontrolünü yap
+                    secondCandy.ControlY(); // Y ekseni kontrolünü yap
 
-                    StartCoroutine(firstCandy.Destroyer());
-                    StartCoroutine(secondCandy.Destroyer());
+                    StartCoroutine(firstCandy.Destroyer()); // Şekerleri yok etmeyi başlat
+                    StartCoroutine(secondCandy.Destroyer()); // Şekerleri yok etmeyi başlat
 
                     firstCandy = null;
                 }
@@ -116,6 +119,7 @@ public class CandyFold : MonoBehaviour
         }
     }
 
+    // Şekerlerin yerlerini değiştirir
     private void Chance()
     {
         creator.candies[(int)firstCandy.x, (int)firstCandy.y] = secondCandy;
@@ -131,85 +135,84 @@ public class CandyFold : MonoBehaviour
         secondCandy.y = tempY;
     }
 
+    // Şekerin hedef konumuna gitmesini sağlar
     private void LocChance()
     {
         transform.position = Vector3.Lerp(transform.position, targetLocation, 0.2f);
     }
 
+    // X ekseni kontrolünü yapar
     private void ControlX()
-{
-    candy_x.Clear();
-
-    // Sağa doğru kontrol
-    for (int i = (int)x + 1; i < creator.candies.GetLength(0); i++)
     {
-        CandyFold rightCandy = creator.candies[i, (int)y];
-        if (rightCandy != null && Colour == rightCandy.Colour)
+        candy_x.Clear();
+
+        // Sağa doğru kontrol
+        for (int i = (int)x + 1; i < creator.candies.GetLength(0); i++)
         {
-            candy_x.Add(rightCandy);
+            CandyFold rightCandy = creator.candies[i, (int)y];
+            if (rightCandy != null && Colour == rightCandy.Colour)
+            {
+                candy_x.Add(rightCandy);
+            }
+            else
+            {
+                break; // Renk değiştiğinde döngüyü sonlandır
+            }
         }
-        else
+
+        // Sola doğru kontrol
+        for (int i = (int)x - 1; i >= 0; i--)
         {
-            break; // Renk değiştiğinde döngüyü sonlandır
+            CandyFold leftCandy = creator.candies[i, (int)y];
+            if (leftCandy != null && Colour == leftCandy.Colour)
+            {
+                candy_x.Add(leftCandy);
+            }
+            else
+            {
+                break; // Renk değiştiğinde döngüyü sonlandır
+            }
         }
     }
 
-    // Sola doğru kontrol
-    for (int i = (int)x - 1; i >= 0; i--)
+    // Y ekseni kontrolünü yapar
+    private void ControlY()
     {
-        CandyFold leftCandy = creator.candies[i, (int)y];
-        if (leftCandy != null && Colour == leftCandy.Colour)
+        candy_y.Clear();
+
+        // Yukarı doğru kontrol
+        for (int i = (int)y + 1; i < creator.candies.GetLength(1); i++)
         {
-            candy_x.Add(leftCandy);
+            CandyFold backCandy = creator.candies[(int)x, i];
+            if (backCandy != null && Colour == backCandy.Colour)
+            {
+                candy_y.Add(backCandy);
+            }
+            else
+            {
+                break; // Renk değiştiğinde döngüyü sonlandır
+            }
         }
-        else
+
+        // Aşağı doğru kontrol
+        for (int i = (int)y - 1; i >= 0; i--)
         {
-            break; // Renk değiştiğinde döngüyü sonlandır
+            CandyFold frontCandy = creator.candies[(int)x, i];
+            if (frontCandy != null && Colour == frontCandy.Colour)
+            {
+                candy_y.Add(frontCandy);
+            }
+            else
+            {
+                break; // Renk değiştiğinde döngüyü sonlandır
+            }
         }
     }
-}
 
-
-   private void ControlY()
-{
-    candy_y.Clear();
-
-    // Yukarı doğru kontrol
-    for (int i = (int)y + 1; i < creator.candies.GetLength(1); i++)
-    {
-        CandyFold backCandy = creator.candies[(int)x, i];
-        if (backCandy != null && Colour == backCandy.Colour)
-        {
-            candy_y.Add(backCandy);
-        }
-        else
-        {
-            break; // Renk değiştiğinde döngüyü sonlandır
-        }
-    }
-
-    // Aşağı doğru kontrol
-    for (int i = (int)y - 1; i >= 0; i--)
-    {
-        CandyFold frontCandy = creator.candies[(int)x, i];
-        if (frontCandy != null && Colour == frontCandy.Colour)
-        {
-            candy_y.Add(frontCandy);
-        }
-        else
-        {
-            break; // Renk değiştiğinde döngüyü sonlandır
-        }
-    }
-}
-
-
+    // Şekerleri yok etmeyi başlatır
     private IEnumerator Destroyer()
     {
-
         yield return new WaitForSeconds(0.3f);
-
-       
 
         if (candy_x.Count >= 2 || candy_y.Count >= 2)
         {
@@ -231,42 +234,37 @@ public class CandyFold : MonoBehaviour
             }
         }
     }
-    
 
+    // Şekerleri yok eder
     public void Disappear()
     {
-        Destroy(gameObject);
-        int controlx=(int)x;
-        int index=0;
+        Destroy(gameObject); // Şekeri yok et
+        int controlx = (int)x;
+        int index = 0;
 
         if (candy_y.Count > 0 || candy_x.Count > 0)
         {
             for (int i = (int)y + 1; i < creator.candies.GetLength(1); i++)
             {
                 CandyFold control = candy_y.Find(e => e.x == controlx && e.y == i);
-                
-                if (control==null)
+
+                if (control == null)
                 {
-                    control = creator.candies[(int)controlx, i];  
+                    control = creator.candies[(int)controlx, i];
                     if (control != null)
                     {
-                    control.NewLoc(controlx, i - (candy_y.Count + 1));
+                        control.NewLoc(controlx, i - (candy_y.Count + 1));
                     }
                 }
-            if (i+1==creator.candies.GetLength(1))
-            {
-                if (index<candy_x.Count)
+                if (i + 1 == creator.candies.GetLength(1))
                 {
-                    
-                    i=(int)y;
-                    controlx=(int)candy_x[index].x;
-                    index++;
-                    
-                    
+                    if (index < candy_x.Count)
+                    {
+                        i = (int)y;
+                        controlx = (int)candy_x[index].x;
+                        index++;
+                    }
                 }
-                
-            }
-                
             }
         }
 
--- Assets/Scripts/creator.cs
+++ Assets/Scripts/creator.cs
@@ -4,41 +4,42 @@ using UnityEngine;
 
 public class creator : MonoBehaviour
 {
-    public GameObject[] sekerler;
-    public int width;
-    public int height;
-    public static CandyFold[ , ] candies;
+    public GameObject[] sekerler; // Bir dizi şekeri temsil eden oyun nesneleri
+    public int width; // Oluşturulan şekerlerin genişliği
+    public int height; // Oluşturulan şekerlerin yüksekliği
+    public static CandyFold[, ] candies; // Şekerleri saklamak için kullanılan bir dizi
+
     void Start()
     {
-        candies = new CandyFold[width,height];
+        candies = new CandyFold[width, height]; // candies dizisini boyutlarına uygun olarak oluştur
+
         for (int a = 0; a < width; a++)
         {
             for (int b = 0; b < height; b++)
             {
-                CreatorCandy(a,b);
+                CreatorCandy(a, b); // Her hücreye bir şeker oluştur
             }
         }
     }
 
     void Update()
     {
-        
+        // Güncelleme işlevi, bu örnekte kullanılmıyor
     }
 
     public void CreatorCandy(int x, int y)
     {
-        GameObject RandomCandyObject= RandomCandy();
-        GameObject newCandy = GameObject.Instantiate(RandomCandyObject,new Vector2(x , y + 10), Quaternion.identity);
-        CandyFold candyFold= newCandy.GetComponent<CandyFold>();
-        candyFold.Colour=RandomCandyObject.name;
-        candyFold.NewLoc(x,y);
-        candies [x,y]=candyFold;
+        GameObject RandomCandyObject = RandomCandy(); // Rastgele bir şeker oyun nesnesi seç
+        GameObject newCandy = GameObject.Instantiate(RandomCandyObject, new Vector2(x, y + 10), Quaternion.identity); // Seçilen şeker nesnesini belirtilen konumda oluştur
+        CandyFold candyFold = newCandy.GetComponent<CandyFold>(); // Oluşturulan nesnenin CandyFold bileşenini al
+        candyFold.Colour = RandomCandyObject.name; // Şekerin rengini belirle (nesnenin adıyla)
+        candyFold.NewLoc(x, y); // Şekerin yeni konumunu ayarla
+        candies[x, y] = candyFold; // Şekeri candies dizisinde sakla
     }
 
     public GameObject RandomCandy()
     {
-        int random= Random.Range(0, sekerler.Length);
-        return sekerler[random];
-
+        int random = Random.Range(0, sekerler.Length); // rastgele bir indeks seç
+        return sekerler[random]; // seçilen indeksteki şeker oyun nesnesini döndür
     }
 }
