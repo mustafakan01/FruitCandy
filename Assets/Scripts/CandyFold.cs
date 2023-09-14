e0c488e3fe3909415ee85b1ebeada838a2ea06af
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-09-14 11:33:19 +0300

    Candy

--- Assets/Scripts/CandyFold.cs
+++ Assets/Scripts/CandyFold.cs
@@ -4,183 +4,246 @@ using UnityEngine;
 
 public class CandyFold : MonoBehaviour
 {
-    public float x,y;
-
+    public float x, y;
     public string Colour;
-    GameObject secim;
 
-    public bool locationChance=false;
-    bool fall=true;
+    private GameObject secim;
+    private Animator animator;
+
+    public bool locationChance = false;
+    private bool fall = true;
 
     public static CandyFold firstCandy;
     public static CandyFold secondCandy;
 
     public Vector3 targetLocation;
 
-    public List<CandyFold> candy_x;
-    public List<CandyFold> candy_y;
-    
-    void Start()
+    private List<CandyFold> candy_x = new List<CandyFold>();
+    private List<CandyFold> candy_y = new List<CandyFold>();
+
+    private void Start()
     {
-        secim= GameObject.FindGameObjectWithTag("Secim");
+        secim = GameObject.FindGameObjectWithTag("Secim");
+        animator = GetComponent<Animator>();
     }
 
-   
-    void Update()
+    private void Update()
     {
-        
-        if (fall==true)
+        if (fall)
         {
-            if (transform.position.y-y<0.01f)
-        {
-            fall=false;
-            transform.position= new Vector3(x,y,0);
+            locationChance=false;
+            if (Mathf.Abs(transform.position.y - y) < 0.01f)
+            {
+                fall = false;
+                transform.position = new Vector3(x, y, 0);
+            }
+            transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, 0), Time.deltaTime * 2f);
         }
-            transform.position=Vector3.Lerp( transform.position, new Vector3(x,y,0),Time.deltaTime*2f);
-        }
+
         if (locationChance)
         {
             LocChance();
         }
     }
 
-    public void NewLoc(float _x, float _y)
-    {
-        x=_x;
-        y=_y;
-    }
+ public void NewLoc(float _x, float _y)
+{
+    // Eski konumu temizle
+    creator.candies[(int)x, (int)y] = null;
 
-    private void OnMouseDown() 
+    // Yeni konumu ayarla
+    x = _x;
+    y = _y;
+
+    // Yeni konumu güncelle
+    creator.candies[(int)x, (int)y] = this;
+
+    // Şekerin düşme durumunu sıfırla
+    fall = true;
+}
+
+
+    private void OnMouseDown()
     {
-        secim.transform.position= transform.position;
+        secim.transform.position = transform.position;
         CandyControl();
-        
     }
 
-    void CandyControl()
-    
+    private void CandyControl()
     {
-        if (firstCandy==null)
+        if (firstCandy == null)
         {
-            firstCandy=this;
+            firstCandy = this;
         }
         else
         {
-            secondCandy=this;
-            if (firstCandy!=secondCandy)
+            secondCandy = this;
+            if (firstCandy != secondCandy)
             {
-                float exraction_x=Mathf.Abs(firstCandy.x-secondCandy.x);
-                float exraction_y=Mathf.Abs(firstCandy.y-secondCandy.y);
+                float exraction_x = Mathf.Abs(firstCandy.x - secondCandy.x);
+                float exraction_y = Mathf.Abs(firstCandy.y - secondCandy.y);
 
-                if (exraction_x+exraction_y==1)
+                if (exraction_x + exraction_y == 1)
                 {
-                    //yer değiştirme
                     Debug.Log("Değiş");
 
-                    firstCandy.targetLocation=secondCandy.transform.position;
-                    secondCandy.targetLocation=firstCandy.transform.position;
-                    firstCandy.locationChance=true;
-                    secondCandy.locationChance=true;
-
-                    
-
-                    
+                    firstCandy.targetLocation = secondCandy.transform.position;
+                    secondCandy.targetLocation = firstCandy.transform.position;
+                    firstCandy.locationChance = true;
+                    secondCandy.locationChance = true;
 
                     Chance();
+
                     firstCandy.ControlX();
                     firstCandy.ControlY();
-                    
+                    secondCandy.ControlX();
+                    secondCandy.ControlY();
 
-                    firstCandy=null;
-                    
+                    StartCoroutine(firstCandy.Destroyer());
+                    StartCoroutine(secondCandy.Destroyer());
+
+                    firstCandy = null;
                 }
                 else
                 {
-                    firstCandy=secondCandy;
-                 
+                    firstCandy = secondCandy;
                 }
             }
-           
-            
-                secondCandy=null;
-            
-        }
 
-        
-    }
-   
-   void Chance()
-   {
-        creator.candies[(int)firstCandy.x,(int)firstCandy.y]=secondCandy;
-        creator.candies[(int)secondCandy.x,(int)secondCandy.y]=firstCandy;
-
-        float firstCandy_x=firstCandy.x;
-        float firstCandy_y=firstCandy.y;
-
-        firstCandy.x=secondCandy.x;
-        firstCandy.y=secondCandy.y;
-
-        secondCandy.x=firstCandy.x;
-        secondCandy.y=firstCandy.y;
-   }
-    void LocChance()
-        {
-            transform.position=Vector3.Lerp(transform.position,targetLocation,0.2f);
-        }
-
-    void ControlX()
-    {
-        for (int i = (int)x+1; i < creator.candies.GetLength(0); i++)
-        {
-            CandyFold rightCandy=creator.candies[i,(int)y];
-            if (Colour==rightCandy.Colour)
-            {
-                candy_x.Add(rightCandy);
-            }
-            else
-            {
-                break;
-            }
-        }
-        for (int i = (int)x-1; i > 0; i--)
-        {
-            CandyFold rightCandy=creator.candies[i,(int)y];
-            if (Colour==rightCandy.Colour)
-            {
-                candy_x.Add(rightCandy);
-            }
-            else
-            {
-                break;
-            }
+            secondCandy = null;
         }
     }
-     void ControlY()
+
+    private void Chance()
     {
-        for (int i = (int)y+1; i < creator.candies.GetLength(0); i++)
+        creator.candies[(int)firstCandy.x, (int)firstCandy.y] = secondCandy;
+        creator.candies[(int)secondCandy.x, (int)secondCandy.y] = firstCandy;
+
+        float tempX = firstCandy.x;
+        float tempY = firstCandy.y;
+
+        firstCandy.x = secondCandy.x;
+        firstCandy.y = secondCandy.y;
+
+        secondCandy.x = tempX;
+        secondCandy.y = tempY;
+    }
+
+    private void LocChance()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetLocation, 0.2f);
+    }
+
+    private void ControlX()
+{
+    candy_x.Clear();
+
+    // Sağa doğru kontrol
+    for (int i = (int)x + 1; i < creator.candies.GetLength(0); i++)
+    {
+        CandyFold rightCandy = creator.candies[i, (int)y];
+        if (rightCandy != null && Colour == rightCandy.Colour)
         {
-            CandyFold backCandy=creator.candies[(int)x,i];
-            if (Colour==backCandy.Colour)
-            {
-                candy_y.Add(backCandy);
-            }
-            else
-            {
-                break;
-            }
+            candy_x.Add(rightCandy);
         }
-        for (int i = (int)y-1; i > 0; i--)
+        else
         {
-            CandyFold backCandy=creator.candies[(int)x,i];
-            if (Colour==backCandy.Colour)
-            {
-                candy_y.Add(backCandy);
-            }
-            else
-            {
-                break;
-            }
+            break; // Renk değiştiğinde döngüyü sonlandır
+        }
+    }
+
+    // Sola doğru kontrol
+    for (int i = (int)x - 1; i >= 0; i--)
+    {
+        CandyFold leftCandy = creator.candies[i, (int)y];
+        if (leftCandy != null && Colour == leftCandy.Colour)
+        {
+            candy_x.Add(leftCandy);
+        }
+        else
+        {
+            break; // Renk değiştiğinde döngüyü sonlandır
         }
     }
 }
 
+
+   private void ControlY()
+{
+    candy_y.Clear();
+
+    // Yukarı doğru kontrol
+    for (int i = (int)y + 1; i < creator.candies.GetLength(1); i++)
+    {
+        CandyFold backCandy = creator.candies[(int)x, i];
+        if (backCandy != null && Colour == backCandy.Colour)
+        {
+            candy_y.Add(backCandy);
+        }
+        else
+        {
+            break; // Renk değiştiğinde döngüyü sonlandır
+        }
+    }
+
+    // Aşağı doğru kontrol
+    for (int i = (int)y - 1; i >= 0; i--)
+    {
+        CandyFold frontCandy = creator.candies[(int)x, i];
+        if (frontCandy != null && Colour == frontCandy.Colour)
+        {
+            candy_y.Add(frontCandy);
+        }
+        else
+        {
+            break; // Renk değiştiğinde döngüyü sonlandır
+        }
+    }
+}
+
+
+    private IEnumerator Destroyer()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        if (candy_x.Count >= 2 || candy_y.Count >= 2)
+        {
+            animator.SetBool("yok_ol", true);
+
+            if (candy_x.Count >= 2)
+            {
+                foreach (var item in candy_x)
+                {
+                    item.animator.SetBool("yok_ol", true);
+                }
+            }
+            else
+            {
+                foreach (var item in candy_y)
+                {
+                    item.animator.SetBool("yok_ol", true);
+                }
+            }
+        }
+    }
+
+    public void Disappear()
+    {
+        Destroy(gameObject);
+
+        if (candy_y.Count > 0 || candy_x.Count > 0)
+        {
+            for (int i = (int)y + 1; i < creator.candies.GetLength(1); i++)
+            {
+                CandyFold control = candy_y.Find(e => e.x == x && e.y == i);
+                control = creator.candies[(int)x, i];
+                if (control != null)
+                {
+                    control.NewLoc(x, i - (candy_y.Count + 1));
+                }
+            }
+        }
+
+        Debug.Log("Yok oldu");
+    }
+}
